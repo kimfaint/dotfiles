99baddae256e1ca024ed048580f19f7e024afb52
Kim Faint <kim.faint@gmail.com>
2016-08-26 11:21:31 +1000

    Added install script.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Make symbolic links to all dotfiles from home directory.
+#
+# Warning: if a dotfile already exists in home dir it will be removed.
+
+cd "$(dirname "$0")"
+dotfiledir=`pwd`
+
+# all files beginning with a dot but not . .. or .git
+dotfiles=`ls -a | grep '^\.' | grep -v '^\.*$' | grep -v '^\.git$'`
+
+for f in $dotfiles; do
+    # if file exists, remove it
+    if [ -e ~/$f ]; then
+        rm -v -R ~/$f
+    fi
+    # copy to homedir (by making symbolic link from homedir)
+    cp -v -sR $dotfiledir/$f ~/$f
+done
