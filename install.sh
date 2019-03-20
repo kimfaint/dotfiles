4e12b461b4c64ca0ee54f2149c326b2273084288
Kim Faint <kim.faint@gmail.com>
2019-03-21 08:25:59 +1000

    added install of dotdirs

--- install.sh
+++ install.sh
@@ -10,17 +10,29 @@ dotfiledir=`pwd`
 # all files beginning with a dot but not . .. or .git
 dotfiles=`ls -a | grep '^\.' | grep -v '^\.*$' | grep -v '^\.git$'`
 
+dotdirs=".config/dunst .config/i3"
+
+echo "installing dotfiles..."
 for f in $dotfiles; do
 
     # skip directories
     if [ -d "$f" ]; then
+        echo "skip $f dir"
         continue
     fi
 
     # if file exists, remove it
     if [ -e ~/$f ]; then
+        echo "remove $f"
         rm -v -R ~/$f
     fi
     # copy to homedir (by making symbolic link from homedir)
+    echo "copy by symlink $f"
     cp -v -sR $dotfiledir/$f ~/$f
+done
+
+echo "installing dotdirs"
+for d in $dotdirs; do
+    echo "linking $d"
+    ln -v -sf $dotfiledir/$d ~/$d
 done
