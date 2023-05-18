98ad8a4fe111f0c2de80628dd7af978eafa0a2be
Kim Faint <kim.faint@gmail.com>
2023-05-18 13:31:08 +1000

    fixed neovim LSP config keybindings and pythonpath detection

--- .config/nvim/lua/my/plugin_config/nvim-cmp.lua
+++ .config/nvim/lua/my/plugin_config/nvim-cmp.lua
@@ -3,11 +3,38 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lspconfig = require('lspconfig')
 
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local my_on_attach = function(client, bufnr)
+  print("my_on_attach")
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'pylsp', 'robotframework_ls' }
+local servers = { 'clangd', 'pylsp' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    -- on_attach = my_custom_on_attach,
+    on_attach = my_on_attach,
     capabilities = capabilities,
   }
 end
@@ -26,9 +53,14 @@ require('lspconfig')['pylsp'].setup {
 }
 
 local pythonpath = os.getenv("PYTHONPATH")
-local pythonexe = os.execute("which python")
+local handle = io.popen('which python')
+local output = handle:read('*a')
+local format = output:gsub('[\n\r]', '')
+local pythonexe = format
+print("pythonexe:", pythonexe)
 
 require('lspconfig')['robotframework_ls'].setup {
+    on_attach = my_on_attach,
     capabilities = capabilities,
     settings = {
         robot = {
@@ -36,8 +68,7 @@ require('lspconfig')['robotframework_ls'].setup {
                 pythonpath
             },
             python = {
-                -- executable = pythonexe
-                executable = "/home/kim/.local/share/virtualenvs/test-wccZTxFl/bin/python"
+                executable = pythonexe
             }
         }
     }
